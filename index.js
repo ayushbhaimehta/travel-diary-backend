edaf9efa1f652cb68c3a4a29af89c7bca8965d3d
Ayush Mehta <ayushbhaimehta20002@gmail.com>
2022-07-07 16:56:12 +0530

    make it better

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
-const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('App is running');
 });
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
