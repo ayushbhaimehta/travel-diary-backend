c7f99a9651084a35819fb8686b623d0f3ee75485
Ayush Mehta <ayushbhaimehta20002@gmail.com>
2022-07-10 01:36:42 +0530

    backend deployment

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
-const MONGODB_URL = "mongodb+srv://ayush1:najdorfdefence@cluster0.efrghqz.mongodb.net/?retryWrites=true&w=majority"
 app.get('/', (req, res) => {
     res.send('App is running');
 });
 
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
